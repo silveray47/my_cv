7ae4e3f83799b9956c3f4122bcbe66d9d76cbd0e
raymond agarunov <silveray47@gmail.com>
2022-08-10 10:48:06 +0300

    first

---
+++ app.js
@@ -0,0 +1,6 @@
+function mobilemenuOnClick() {
+  document.getElementById("mobile-menu").classList.toggle("change");
+  document.getElementById("mobile-menu-bar").classList.toggle("change");
+  document.getElementById("nav").classList.toggle("change-nav");
+  document.getElementById("mobile-menu-bg").classList.toggle("change-bg");
+}
